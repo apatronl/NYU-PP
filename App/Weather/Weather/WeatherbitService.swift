ee6b99b4bb4837ba0f45ba29423b08ba57279445
Alejandrina Patrón <alepatronlopez@gmail.com>
2025-04-04 22:38:06 -0400

    Cleanup

--- App/Weather/Weather/WeatherbitService.swift
+++ App/Weather/Weather/WeatherbitService.swift
@@ -11,7 +11,7 @@ class WeatherbitService: WeatherService {
 
   // Get your API key at https://www.weatherbit.io/
   // Default temperature unit is Celsius, but can be changed to Farenheit by adding `units=I` to the url
-  private let apiKey = "c51aedcf931d42ac84a4f893f4b7fc8b"
+  private let apiKey = "YOUR_API_KEY"
   private let baseURL = "https://api.weatherbit.io/v2.0/"
   private let nyc = (lat: 40.71, lon: -74)
   private let decoder = JSONDecoder()
